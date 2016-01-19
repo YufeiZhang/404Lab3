a5094eef117e6ababa094e826b7579247a3a5958
YufeiZhang <philipyufei@gmail.com>
2016-01-19 09:21:18 -0700

    404 Tuesday lab 3

--- cgi-program.py
+++ cgi-program.py
@@ -6,7 +6,6 @@ import sys, os, cgi
 
 print("Blah!", file=sys.stderr)  # this is printed on terminal
 
-
 ''' This is the HTMP version '''
 print("Content-type: text/html")# this is the header of a webpage
 print("")
@@ -14,7 +13,6 @@ print("")
 print("<HTML><BODY><FORM method='POST'> <INPUT name='x'>")
 print("<br><br>Hello, this is the cgi script<br><br>") # this is what we print on web
 print("I am process %i<br><br>" % os.getpid())
-
 
 print(os.environ)
 
